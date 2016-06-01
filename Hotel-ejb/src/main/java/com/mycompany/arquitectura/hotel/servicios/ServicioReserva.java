5c60000d917b87dc3fd9c45f3d11b5157143a2a1
Dalia22 <dksoleo_22@hotmail.com>
2016-06-01 06:57:20 -0500

    7to (Falta corregir precios) Ya le corrió a Dennys!!:D

---
+++ Hotel-ejb/src/main/java/com/mycompany/arquitectura/hotel/servicios/ServicioReserva.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.arquitectura.hotel.servicios;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author LENOVO
+ */
+@LocalBean
+@Stateless
+public class ServicioReserva {
+    
+    
+    
+}
--- Hotel-web/src/main/java/com/mycompany/ws/ServicioWebHotel.java
+++ Hotel-web/src/main/java/com/mycompany/ws/ServicioWebHotel.java
@@ -7,14 +7,13 @@ package com.mycompany.ws;
 
 import com.mycompany.arquitectura.hotel.servicios.ServicioHotel;
 import com.mycompany.arquitectura.hotel.util.RespDisponibilidad;
-import java.util.Date;
+import java.math.BigDecimal;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.ejb.Stateless;
-import javax.xml.datatype.XMLGregorianCalendar;
 
 /**
  *
@@ -32,5 +31,14 @@ public class ServicioWebHotel {
                                                                          @WebParam(name = "totalPersonas") Integer totalPersonas, @WebParam(name = "incluyeDesayuno") Boolean incluyeDesayuno) {
         System.out.println("fEntrada: "+fechaEntrada);
         return shotel.consulta1(fechaEntrada, fechaSalida, totalPersonas, incluyeDesayuno);
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "reservaHabitacionHotel")
+    public String reservaHabitacionHotel(@WebParam(name = "fechaEntrada") String fechaEntrada, @WebParam(name = "fechaSalida") String fechaSalida, @WebParam(name = "total_personas") Integer total_personas, @WebParam(name = "desayuno") Boolean desayuno, @WebParam(name = "precio") BigDecimal precio, @WebParam(name = "codigosHabitantes") List<String> codigosHabitantes) {
+        //TODO write your implementation code here:
+        return null;
     }
 }
